946de00ad2012113a7892cabfa223ab457b20bf0
eyalnesher <eyal.nesher@gmail.com>
2018-09-01 19:17:12 +0300

    Update header

--- src/stego.h
+++ src/stego.h
@@ -2,6 +2,16 @@
 #ifndef BS_STEGO_H
 #define BS_STEGO_H
 
-// Code
+#include <stdlib.h>
+
+#define STEGO_KEY_SIZE 128
+
+int stego_read_level(const void* key, const void* disk, size_t level_size,
+  void* buf, off_t off, size_t size);
+
+int stego_write_level(const void* key, void* disk, size_t level_size,
+  const void* buf, off_t off, size_t size);
+
+int stego_gen_keys(void* buf, int count);
 
 #endif
