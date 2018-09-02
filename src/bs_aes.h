5a6e8bc58aec152e9759707ae83ebff7ec4cf300
Noam Raz <noamraz8@gmail.com>
2018-09-02 15:57:28 +0200

    Use angle brackets when including external libraries

--- src/bs_aes.h
+++ src/bs_aes.h
@@ -1,7 +1,6 @@
 
 #ifndef BS_AES_H
 #define BS_AES_H
-#include "openssl/bio.h"
-#include "openssl/ssl.h"
-#include "openssl/err.h"
-#endif
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
