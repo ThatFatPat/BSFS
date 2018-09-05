ddd88807e87a3d723d5551de28cdcd944343c649
Noam Raz <noamraz8@gmail.com>
2018-09-05 23:40:16 +0300

    Add unit tests for bft_entry_init

--- tests/test_bft.c
+++ tests/test_bft.c
@@ -1,17 +1,42 @@
 #include "test_bft.h"
 
-START_TEST(test_example)
+#include "bft.h"
+#include <string.h>
+
+START_TEST(test_bft_init_basic)
 {
-  ck_assert_int_eq(3, 3);
+  bft_entry_t ent;
+  bft_entry_init(&ent, "Test", 42, 0, 1, 2, 3);
+  
+  ck_assert_str_eq(ent.name, "Test");
+  ck_assert_uint_eq(ent.size, 42);
+  ck_assert_uint_eq(ent.mode, 0);
+  ck_assert_uint_eq(ent.initial_cluster, 1);
+  ck_assert_uint_eq(ent.atim, 2);
+  ck_assert_uint_eq(ent.mtim, 3);
+
+  bft_entry_destroy(&ent);
 }
 END_TEST
 
+START_TEST(test_bft_init_deep_copy)
+{
+  const char* name = "entry";
+  bft_entry_t ent;
+  bft_entry_init(&ent, name, 0, 0, 0, 0, 0);
+  ck_assert_ptr_ne(ent.name, name);
+  bft_entry_destroy(&ent);
+}
+END_TEST
+
+
 Suite* bft_suite(void) {
   Suite* suite = suite_create("bft");
-  TCase* example_tcase = tcase_create("example");
-
-  tcase_add_test(example_tcase, test_example);
-  suite_add_tcase(suite, example_tcase);
+  
+  TCase* lifetime_tc = tcase_create("lifetime");
+  tcase_add_test(lifetime_tc, test_bft_init_basic);
+  tcase_add_test(lifetime_tc, test_bft_init_deep_copy);
+  suite_add_tcase(suite, lifetime_tc);
 
   return suite;
 }
