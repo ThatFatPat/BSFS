90425bf0c49582a283b6a2bcf2ca9f0fb8948efc
Iddo Shavit <idoshav@gmail.com>
2018-09-05 21:41:26 +0300

    Define api for AES

--- src/enc.c
+++ src/enc.c
@@ -1,1 +1,17 @@
 #include "enc.h"
+#include <openssl/aes.h>
+#include <openssl/evp.h>
+
+#define NROUNDS 5
+
+static int gen_key(const void* password, size_t password_size, void* key, void* iv) {
+  return 0;
+}
+
+int aes_encrypt(const void* password, size_t password_size, const void* data, size_t size, void** buf_pointer, size_t* buf_size) {
+  return 0;
+}
+
+int aes_decrypt(const void* password, size_t password_size, const void* enc, size_t size, void** buf_pointer, size_t* buf_size) {
+  return 0;
+}
