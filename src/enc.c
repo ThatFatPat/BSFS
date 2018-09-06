6f8e4a6511f95c378d44097c59b91927813a20fd
Iddo Shavit <idoshav@gmail.com>
2018-09-06 18:19:03 +0300

    Fix the gen_key method. Remove testing of static (private) function.

--- src/enc.c
+++ src/enc.c
@@ -1,13 +1,13 @@
 #include "enc.h"
 #include <openssl/aes.h>
 #include <openssl/evp.h>
+#include <errno.h>
 
 #define NROUNDS 5
 
+
 static int gen_key(const void* password, size_t password_size, void* key, void* iv) {
- 	int res_size;
- 	key = malloc(sizeof(unsigned char) * 16);
-	iv = malloc(sizeof(unsigned char) * 16);
+  int res_size;
  	/*
    * Gen key & IV for AES 128 CBC mode. A SHA1 digest is used to hash the supplied key material.
    * nrounds is the number of times the we hash the material. More rounds are more secure but
@@ -21,9 +21,9 @@ static int gen_key(const void* password, size_t password_size, void* key, void* 
 }
 
 int aes_encrypt(const void* password, size_t password_size, const void* data, size_t size, void** buf_pointer, size_t* buf_size) {
-  return 0;
+  return -ENOSYS;
 }
 
 int aes_decrypt(const void* password, size_t password_size, const void* enc, size_t size, void** buf_pointer, size_t* buf_size) {
-  return 0;
+  return -ENOSYS;
 }
--- tests/test_enc.c
+++ tests/test_enc.c
@@ -1,21 +1,14 @@
 #include "test_enc.h"
-#include "../src/enc.c"
+#include "enc.h"
 #include <string.h>
 
-START_TEST(test_gen_key_pass)
-{
-	void *key, *iv;
-	const void* password = "Who are you?\n I'm the Doctor.\n Doctor who? \n Precisely.";
-	ck_assert_int_eq(gen_key(password, sizeof(password), key, iv), 0);
-}
-END_TEST
 
 Suite* enc_suite(void) {
   Suite* suite = suite_create("enc");
-  TCase* gen_key_tcase = tcase_create("Gen Key");
+  //TCase* gen_key_tcase = tcase_create("Gen Key");
 
-  tcase_add_test(gen_key_tcase, test_gen_key_pass);
-  suite_add_tcase(suite, gen_key_tcase);
+  //tcase_add_test(gen_key_tcase, test_gen_key_pass);
+  //suite_add_tcase(suite, gen_key_tcase);
 
   return suite;
 }
