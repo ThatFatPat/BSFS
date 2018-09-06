212c072348999d501a63c19f595befdfa7401f30
Iddo Shavit <idoshav@gmail.com>
2018-09-06 23:13:17 +0300

    Implement AES API

--- src/enc.c
+++ src/enc.c
@@ -1,10 +1,11 @@
 #include "enc.h"
+#include <errno.h>
+#include <stdint.h>
 #include <openssl/aes.h>
 #include <openssl/evp.h>
-#include <errno.h>
+
 
 #define NROUNDS 5
-
 
 static int gen_key(const void* password, size_t password_size, void* key, void* iv) {
   int res_size;
@@ -13,7 +14,8 @@ static int gen_key(const void* password, size_t password_size, void* key, void* 
    * nrounds is the number of times the we hash the material. More rounds are more secure but
    * slower.
    */
-  res_size = EVP_BytesToKey(EVP_aes_128_cbc(), EVP_sha1(), NULL, password, password_size, NROUNDS, key, iv);
+  res_size = EVP_BytesToKey(EVP_aes_128_cbc(), EVP_sha1(), NULL, (uint8_t*) password,
+   password_size, NROUNDS, (uint8_t*) key, (uint8_t*) iv);
   if (res_size != 16) {
     return -1;
   }
@@ -21,9 +23,89 @@ static int gen_key(const void* password, size_t password_size, void* key, void* 
 }
 
 int aes_encrypt(const void* password, size_t password_size, const void* data, size_t size, void** buf_pointer, size_t* buf_size) {
-  return -ENOSYS;
+  int ret = 0;
+  EVP_CIPHER_CTX* e_ctx = EVP_CIPHER_CTX_new();
+
+  if (!e_ctx) {
+    return -ENOMEM;
+  }
+
+  uint8_t key[16], iv[16];
+  ret = gen_key(password, password_size, key, iv);
+
+  if (ret != 0) {
+    goto cleanup_ctx;
+  }
+  
+  if (!EVP_EncryptInit_ex(e_ctx, EVP_aes_128_cbc(), NULL, key, iv)) {
+    ret = -1;
+    goto cleanup_ctx;
+  }
+
+  int content_size = 0, final_size = 0;
+  uint8_t* ciphertext = (uint8_t*) malloc(size + AES_BLOCK_SIZE - 1);
+
+  if (!EVP_EncryptUpdate(e_ctx, ciphertext, &content_size, (const uint8_t*) data, size)) {
+    ret = -1;
+    free(ciphertext);
+    goto cleanup_ctx;
+  }
+
+  if (!EVP_EncryptFinal_ex(e_ctx, ciphertext + content_size, &final_size)) {
+    ret = -1;
+    free(ciphertext);
+    goto cleanup_ctx;
+  }
+
+  *buf_size = content_size + final_size;
+  *buf_pointer = ciphertext;
+
+  cleanup_ctx:
+  EVP_CIPHER_CTX_free(e_ctx);
+
+  return ret;
 }
 
 int aes_decrypt(const void* password, size_t password_size, const void* enc, size_t size, void** buf_pointer, size_t* buf_size) {
-  return -ENOSYS;
+  int ret = 0;
+  EVP_CIPHER_CTX* d_ctx = EVP_CIPHER_CTX_new();
+
+  if (!d_ctx) {
+    return -ENOMEM;
+  }
+
+  uint8_t key[16], iv[16];
+  ret = gen_key(password, password_size, key, iv);
+
+  if (ret != 0) {
+    goto cleanup_ctx;
+  }
+  
+  if (!EVP_DecryptInit_ex(d_ctx, EVP_aes_128_cbc(), NULL, key, iv)) {
+    ret = -1;
+    goto cleanup_ctx;
+  }
+
+  int content_size = 0, final_size = 0;
+  uint8_t* plaintext = (uint8_t*) malloc(size);
+
+  if (!EVP_DecryptUpdate(d_ctx, plaintext, &content_size, (const uint8_t*) enc, size)) {
+    ret = -1;
+    free(plaintext);
+    goto cleanup_ctx;
+  }
+
+  if (!EVP_DecryptFinal_ex(d_ctx, plaintext + content_size, &final_size)) {
+    ret = -1;
+    free(plaintext);
+    goto cleanup_ctx;
+  }
+
+  *buf_size = content_size + final_size;
+  *buf_pointer = plaintext;
+
+  cleanup_ctx:
+  EVP_CIPHER_CTX_free(d_ctx);
+
+  return ret;
 }
--- tests/test_enc.c
+++ tests/test_enc.c
@@ -1,14 +1,39 @@
 #include "test_enc.h"
 #include "enc.h"
+#include <stdlib.h>
 #include <string.h>
 
 
+START_TEST(test_roundtrip1)
+{
+  const char* password = "Doctor Who";
+  const char* plain = "But what kind of Doctor?";
+  
+  void* cipher = NULL;
+  size_t cipher_size = 0;
+  int status = aes_encrypt(password, strlen(password), plain, strlen(plain) + 1,
+    &cipher, &cipher_size);
+  ck_assert_int_eq(status, 0);
+
+  void* decrypted = NULL;
+  size_t decrypted_size = 0;
+  status = aes_decrypt(password, strlen(password), cipher, cipher_size,
+    &decrypted, &decrypted_size);
+  ck_assert_int_eq(status, 0);
+
+  ck_assert_str_eq(plain, (char*)decrypted);
+
+  free(decrypted);
+  free(cipher);
+}
+END_TEST
+
 Suite* enc_suite(void) {
   Suite* suite = suite_create("enc");
-  //TCase* gen_key_tcase = tcase_create("Gen Key");
+  TCase* roundtrip_tcase = tcase_create("Round Trip");
 
-  //tcase_add_test(gen_key_tcase, test_gen_key_pass);
-  //suite_add_tcase(suite, gen_key_tcase);
+  tcase_add_test(roundtrip_tcase, test_roundtrip1);
+  suite_add_tcase(suite, roundtrip_tcase);
 
   return suite;
 }
