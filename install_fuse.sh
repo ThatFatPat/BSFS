cbf045eada1612ff7b0bbd0a1a8d8ce763fcec4d
ThatFatPat <idoshav@gmail.com>
2018-09-06 09:16:24 +0300

    Script now works and installs fuse-3.2.6

--- install_fuse.sh
+++ install_fuse.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-curl https://github.com/libfuse/libfuse/releases/download/fuse-3.2.5/fuse-3.2.5.tar.xz
-tar xf fuse-3.2.5.tar.xz
-cd fuse-3.2.5
-meson
-ninja build
+wget https://api.github.com/repos/libfuse/libfuse/tarball/fuse-3.2.6
+tar xf fuse-3.2.6
+cd libfuse-libfuse-569e6ba
+meson build
+cd build
+ninja
 sudo ninja install
