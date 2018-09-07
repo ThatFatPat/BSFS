fe5c7773cd82d3958f4f7106db947c89e05d4dc8
Iddo Shavit <idoshav@gmail.com>
2018-09-08 00:40:36 +0300

    Implement disk_free Fixes and structural reorganization of disk_create

--- src/disk_api.c
+++ src/disk_api.c
@@ -8,11 +8,13 @@
 #include <sys/file.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 struct bs_disk_impl {
+  int fd;
   void* data;
   size_t data_size;
-  pthread_rwlock_t lock;
+  pthread_rwlock_t lock; // synchronize access to `data` across threads
 };
 
 int disk_create(int fd, bs_disk_t *disk) {
@@ -24,54 +26,65 @@ int disk_create(int fd, bs_disk_t *disk) {
     return -ENOMEM;                
   }
 
-  flock(fd, LOCK_EX); // Locks the file with file descriptor `fd` to be EXCLUSIVE to THIS FD.
+  if (flock(fd, LOCK_EX) == -1) { // Locks the file with file descriptor `fd` to be EXCLUSIVE to THIS FD.
+    ret = -errno;
+    goto fail_after_alloc;
+  }
 
   struct stat sb;
-  if(fstat(fd, &sb) == -1) {
+  if (fstat(fd, &sb) == -1) {
     ret = -errno;
-    goto fail;
+    goto fail_after_flock;
   }
 
   disk_local->data = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   if (disk_local->data == MAP_FAILED) {
     ret = -errno;                    
-    goto fail;
+    goto fail_after_flock;
+  }
+
+  if (int ret_pthread = pthread_rwlock_init(&disk_local->lock, NULL)) {
+    ret = -ret_pthread;
+    goto fail_after_flock;
   }
 
   disk_local->data_size = sb.st_size;
-  if (int ret_pthread = pthread_rwlock_init(&disk_local->lock, NULL)) {
-    ret = -ret_pthread;
-    goto fail;
-  }
-
+  disk_local->fd = fd;
   *disk = disk_local;
   return 0;
 
-  fail:
-  free(disk_local);
+  // Failure
+  fail_after_flock:
   flock(fd, LOCK_UN);
+  fail_after_alloc:
+  free(disk_local);
   return ret;
 }
 
-void disk_free(bs_disk_t disk){
+void disk_free(bs_disk_t disk) {
+  pthread_rwlock_destroy(&disk->lock);
+  munmap(disk->data, disk->data_size);
+  flock(disk->fd, LOCK_UN);
+  close(disk->fd);
+  free(disk);
 }
 
-size_t disk_get_size(bs_disk_t disk){
-  return disk.data_size;
+size_t disk_get_size(bs_disk_t disk) {
+  return disk->data_size;
 }
 
-int disk_lock_read(bs_disk_t disk, const void **data){
+int disk_lock_read(bs_disk_t disk, const void **data) {
   return -ENOSYS;
 }
 
-int disk_unlock_read(bs_disk_t disk){
+int disk_unlock_read(bs_disk_t disk) {
   return -ENOSYS;
 }
 
-int disk_lock_write(bs_disk_t disk, void **data){
+int disk_lock_write(bs_disk_t disk, void **data) {
   return -ENOSYS;
 }
 
-int disk_unlock_write(bs_disk_t disk){
+int disk_unlock_write(bs_disk_t disk) {
   return -ENOSYS;
 }
