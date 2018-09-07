861f283af56b8a2909004b83664ccc1a75cd2d50
Iddo Shavit <idoshav@gmail.com>
2018-09-07 19:33:10 +0300

    Define C and Header files

--- src/disk_api.c
+++ src/disk_api.c
@@ -1,1 +1,39 @@
 #include "disk_api.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <errno.h>
+
+struct disk {
+  int fd;
+  void* data;
+  size_t data_size;
+  bool read_lock;
+  bool write_lock;
+};
+
+int disk_create(int fd, bs_disk_t *disk){
+  return -ENOSYS;
+}
+
+void disk_free(bs_disk_t disk){
+}
+
+size_t disk_get_size(bs_disk_t disk){
+  return -1;
+}
+
+int disk_lock_read(bs_disk_t disk, const void **data){
+  return -ENOSYS;
+}
+
+int disk_unlock_read(bs_disk_t disk){
+  return -ENOSYS;
+}
+
+int disk_lock_write(bs_disk_t disk, void **data){
+  return -ENOSYS;
+}
+
+int disk_unlock_write(bs_disk_t disk){
+  return -ENOSYS;
+}
--- src/disk_api.h
+++ src/disk_api.h
@@ -1,4 +1,14 @@
 #ifndef BS_DISK_API_H
 #define BS_DISK_API_H
 
+typedef struct disk bs_disk_t;
+
+int disk_create(int fd, bs_disk_t *disk);
+void disk_free(bs_disk_t disk);
+size_t disk_get_size(bs_disk_t disk);
+int disk_lock_read(bs_disk_t disk, const void **data);
+int disk_unlock_read(bs_disk_t disk);
+int disk_lock_write(bs_disk_t disk, void **data);
+int disk_unlock_write(bs_disk_t disk);
+
 #endif //BS_DISK_API_H
