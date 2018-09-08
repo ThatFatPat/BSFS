39cdf59e735b152b73c0cb9966f69127f1309943
ThatFatPat <idoshav@gmail.com>
2018-09-08 18:46:32 +0300

    Remove unecessary script

--- install_fuse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-wget https://github.com/libfuse/libfuse/releases/download/fuse-3.2.6/fuse-3.2.6.tar.xz
-tar xf fuse-3.2.6.tar.xz
-cd fuse-3.2.6
-meson build
-cd build
-ninja
-ninja install
