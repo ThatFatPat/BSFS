f20521557181d34b8092c0cc8ed31e175b505fa9
Noam Raz <noamraz8@gmail.com>
2018-09-09 10:49:43 +0300

    Add endianness helpers

--- src/bft.c
+++ src/bft.c
@@ -1,5 +1,6 @@
 #include "bft.h"
 
+#include <arpa/inet.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,6 +38,18 @@ int bft_find_free_table_entry(const void* bft, bft_offset_t* off) {
     }
   }
   return -ENOSPC;
+}
+
+
+static uint32_t read_big_endian(const void* buf) {
+  uint32_t big_endian;
+  memcpy(&big_endian, buf, sizeof(uint32_t));
+  return ntohl(big_endian);
+}
+
+static void write_big_endian(void* buf, uint32_t host_endian) {
+  uint32_t big_endian = htonl(host_endian);
+  memcpy(buf, &big_endian, sizeof(uint32_t));
 }
 
 int bft_read_table_entry(const void* bft, bft_entry_t* ent, bft_offset_t off) {
