e6bf68d16771f638f11d3c07caae8dd31284aaeb
Felix <flexic.l@gmail.com>
2018-09-11 19:37:09 +0300

    modify stego.c, add coments

--- src/stego.c
+++ src/stego.c
@@ -3,11 +3,25 @@
 #include <limits.h>
 #include <openssl/rand.h>
 
-static int scalar_product(uint64_t a, uint64_t b) {
+static int scalar_product(uint8_t* a, uint8_t* b, int size) { // Computing the scalar product of a and b, with "size" bytes length.
     int out = 0;
-    for (int i = 0; i < 63; i++){
-        if (((a >> i) & 1U)*((b >> i) & 1U) == 1){
-            out = !out;
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < CHAR_BIT; j++){
+            if (((a[i] >> j) & 1U)*((b[i] >> j) & 1U) == 1){
+                out = !out;
+            }
+        }
+    }
+    return out;
+}
+
+static int norm(uint8_t* a, int size){  // Computing the norm of a, with "size" bytes length.
+    int out = 0;
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < CHAR_BIT; j++){
+            if (((a[i] >> j) & 1U) == 1){
+                out = !out;
+            }
         }
     }
     return out;
@@ -15,30 +29,40 @@ static int scalar_product(uint64_t a, uint64_t b) {
 
 
 
-static uint64_t* generate_random_key(void){
-    uint64_t* out = (uint64_t*) malloc(sizeof(uint64_t)*2);
-    uint64_t buf[2];
-    RAND_bytes((unsigned char*) buf, sizeof(buf));
-    uint64_t rnd_int = out[1];
-    rnd_int -= rnd_int%16;
-    out[1] = rnd_int;
-    return out;
+static void generate_random_key(uint8_t* buf){  // Generating a random key. The 2 last bytes are filled with 0.
+    int bytes_in_key = STEGO_KEY_BITS/CHAR_BIT;
+    buf = (uint8_t*) malloc(bytes_in_key);
+    RAND_bytes((unsigned char*) buf, sizeof(buf) - 2);
+    buf[bytes_in_key-2] = 0;
+    buf[bytes_in_key-1] = 0;
 }
 
-int stego_gen_keys(void* buf, int count) {
-    int total_keys_size = count * (STEGO_KEY_BITS/CHAR_BIT);
-    uint64_t * keys = (uint64_t *) malloc(total_keys_size);
-    for (int i = 0; i < count*2; i+=2){
-        uint64_t* rnd_key = generate_random_key();
-        keys[i] = rnd_key[0];
-        keys[i+1] = rnd_key[1];
-        for (int j = 0; j < i; j+=2){
-            uint64_t part_1 = scalar_product(keys[i], keys[j]);
-            uint64_t part_2 = scalar_product(keys[i+1], keys[j+1]);
-            if (part_1 + part_2 == 1){
-                keys[i]+=keys[j];
-                keys[i+1]+=keys[j+1];
+int stego_gen_keys(void* buf, int count) {  // Generating "count" orthonormal keys. Uses special Gram-Schmidt to do so.
+    int key_size = STEGO_KEY_BITS/CHAR_BIT;
+    int total_keys_size = count * (key_size);
+    uint8_t* keys = (uint8_t*) malloc(total_keys_size);
+    for (int i = 0; i < total_keys_size; i+=key_size){ 
+        uint8_t* rnd_key = NULL;
+        generate_random_key(rnd_key);
+        for (int j = 0; j < i; j+=key_size){  // Add to "rnd_key" all the keys before him which have scalar product 1 with him. 
+            int product = scalar_product(rnd_key, &keys[j], STEGO_KEY_BITS/CHAR_BIT);
+            if (product == 1){
+                for (int l = 0; l < key_size; l++) {
+                    rnd_key[l] += keys[j+l];
+                }
             }           
+        }
+        if (norm(rnd_key, key_size) == 1){  // If the norm of the key is 1, set the proper bit in the last two bytes.
+            int key_num = i/key_size;
+            if (key_num < 8) {
+                rnd_key[key_size-2] |= 1UL << key_num;
+            }
+            else {
+                rnd_key[key_size-1] |= 1UL << (key_num - 8);
+            }
+        }
+        for (int k = 0; k < key_size; k++) {     // Insert the key into "keys".
+            keys[i+k] = rnd_key[k];
         }
     }
     buf = keys;
--- tests/test_stego.c
+++ tests/test_stego.c
@@ -1,6 +1,6 @@
 #include "test_stego.h"
 
-START_TEST(test_example)
+START_TEST(test_gen_keys)
 {
   ck_assert_int_eq(3, 3);
 }
@@ -9,8 +9,7 @@ END_TEST
 Suite* stego_suite(void) {
   Suite* suite = suite_create("stego");
   TCase* example_tcase = tcase_create("example");
-
-  tcase_add_test(example_tcase, test_example);
+  tcase_add_test(example_tcase, test_gen_keys);
   suite_add_tcase(suite, example_tcase);
 
   return suite;
