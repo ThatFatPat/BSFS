f27085c2c385137cce8a23ddbba72381296b5ce1
Iddo Shavit <idoshav@gmail.com>
2018-09-11 21:01:35 +0300

    Apply clang-format

--- src/cluster.h
+++ src/cluster.h
@@ -2,8 +2,8 @@
 #define BS_CLUSTER_H
 
 #include "disk.h"
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
 typedef uint32_t cluster_offset_t;
 #define CLUSTER_OFFSET_EOF UINT32_MAX
@@ -12,15 +12,19 @@ typedef uint32_t cluster_offset_t;
 
 size_t fs_count_clusters(size_t level_size);
 
-int fs_read_cluster(const void* key, bs_disk_t disk, void* buf, cluster_offset_t cluster);
-int fs_write_cluster(const void* key, bs_disk_t disk, const void* buf, cluster_offset_t cluster);
+int fs_read_cluster(const void* key, bs_disk_t disk, void* buf,
+                    cluster_offset_t cluster);
+int fs_write_cluster(const void* key, bs_disk_t disk, const void* buf,
+                     cluster_offset_t cluster);
 
 cluster_offset_t fs_next_cluster(const void* cluster);
 
-int fs_read_bitmap(const void* key, bs_disk_t disk, void* buf, size_t bitmap_size);
-int fs_write_bitmap(const void* key, bs_disk_t disk, const void* buf, size_t bitmap_size);
+int fs_read_bitmap(const void* key, bs_disk_t disk, void* buf,
+                   size_t bitmap_size);
+int fs_write_bitmap(const void* key, bs_disk_t disk, const void* buf,
+                    size_t bitmap_size);
 
 int fs_alloc_cluster(void* bitmap, cluster_offset_t* new_cluster);
 int fs_dealloc_cluster(void* bitmap, cluster_offset_t cluster);
 
-#endif //BS_CLUSTER_H
+#endif // BS_CLUSTER_H
