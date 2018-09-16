7d39e00eade2c02fae4957e0aa75a71f11c9869b
Felix <flexic.l@gmail.com>
2018-09-16 19:28:09 +0300

    Add test for stego_gen_keys, correct stego.c

--- src/stego.c
+++ src/stego.c
@@ -1,72 +1,75 @@
 #include "stego.h"
-#include <stdint.h>
 #include <limits.h>
 #include <openssl/rand.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
-int count_bits(uint8_t a) {
-    int ret = 0;
-    while (a) {
-        ret++;
-        a &= a - 1; // move to next bit
-    }
-    return ret;
+static int count_bits(uint8_t a) {
+  int ret = 0;
+  while (a) {
+    ret++;
+    a &= a - 1; // move to next bit
+  }
+  return ret;
 }
 
-bool scalar_product(const uint8_t* a, const uint8_t* b, size_t size) {
-    bool ret = 0;
-    for (size_t i = 0; i < size; i++) {
-        ret ^= count_bits(a[i] & b[i]) & 1;
-    }
-    return ret;
+static bool scalar_product(const uint8_t* a, const uint8_t* b, size_t size) {
+  bool ret = 0;
+  for (size_t i = 0; i < size; i++) {
+    ret ^= count_bits(a[i] & b[i]) & 1;
+  }
+  return ret;
 }
 
-static int norm(uint8_t* a, size_t size){  // Computing the norm of a, with "size" bytes length.
-    return scalar_product(a, a, size);
+static bool
+norm(uint8_t* a,
+     size_t size) { // Computing the norm of a, with "size" bytes length.
+  return scalar_product(a, a, size);
 }
 
-
-
-static int generate_random_key(uint8_t* buf){  // Generating a random key. The 2 last bytes are filled with 0.
-    int bytes_in_key = STEGO_KEY_BITS/CHAR_BIT;
-    if (RAND_bytes((unsigned char*) buf, bytes_in_key-2) == 0){
-        return -1;
-    }
-    memset(buf + bytes_in_key - 2, 0, 2);
-    return 0;
+static int generate_random_key(uint8_t* buf) { // Generating a random key. The 2
+                                               // last bytes are filled with 0.
+  int bytes_in_key = STEGO_KEY_BITS / CHAR_BIT;
+  if (RAND_bytes((unsigned char*) buf, bytes_in_key - 2) == 0) {
+    return -1;
+  }
+  memset(buf + bytes_in_key - 2, 0, 2);
+  return 0;
 }
 
-int stego_gen_keys(void* buf, int count) {  // Generating "count" orthonormal keys. Uses special Gram-Schmidt to do so.
-    size_t key_size = STEGO_KEY_BITS/CHAR_BIT;
-    size_t total_keys_size = count * (key_size);
-    uint8_t* int_buf = (uint8_t*) buf;
-    for (size_t i = 0; i < total_keys_size; i+=key_size){ 
-        uint8_t* rnd_key = int_buf + i;
-        if (generate_random_key(rnd_key) == -1){
-            return -1;
-        }
-        for (size_t j = 0; j < i; j+=key_size){  // Add to "rnd_key" all the keys before him which have scalar product 1 with him. 
-            int product = scalar_product(rnd_key, int_buf + j, STEGO_KEY_BITS/CHAR_BIT);
-            if (product == 1){
-                for (size_t l = 0; l < key_size; l++) {
-                    rnd_key[l] ^= int_buf[j+l];
-                }
-            }           
-        }
-        if (norm(rnd_key, key_size) == 1){  // If the norm of the key is 1, set the proper bit in the last two bytes.
-            int key_num = i/key_size;
-            if (key_num < 8) {
-                rnd_key[key_size-2] |= 1UL << key_num;
-            }
-            else {
-                rnd_key[key_size-1] |= 1UL << (key_num - 8);
-            }
-        }
-        for (size_t k = 0; k < key_size; k++) {     // Insert the key into "keys".
-            int_buf[i+k] = rnd_key[k];
-        }
+int stego_gen_keys(void* buf,
+                   int count) { // Generating "count" orthonormal keys. Uses
+                                // special Gram-Schmidt to do so.
+  size_t key_size = STEGO_KEY_BITS / CHAR_BIT;
+  size_t total_keys_size = count * (key_size);
+  uint8_t* int_buf = (uint8_t*) buf;
+  for (size_t i = 0; i < total_keys_size; i += key_size) {
+    uint8_t* rnd_key = int_buf + i;
+    if (generate_random_key(rnd_key) == -1) {
+      return -1;
     }
-    return 0;
+    for (size_t j = 0; j < i;
+         j += key_size) { // Add to "rnd_key" all the keys before him which have
+                          // scalar product 1 with him.
+      int product =
+          scalar_product(rnd_key, int_buf + j, STEGO_KEY_BITS / CHAR_BIT);
+      if (product == 1) {
+        for (size_t l = 0; l < key_size; l++) {
+          rnd_key[l] ^= int_buf[j + l];
+        }
+      }
+    }
+    if (norm(rnd_key, key_size) == 0) { // If the norm of the key is 0, set the
+                                        // proper bit in the last two bytes.
+      int key_num = i / key_size;
+      if (key_num < 8) {
+        rnd_key[key_size - 2] |= 1UL << key_num;
+      } else {
+        rnd_key[key_size - 1] |= 1UL << (key_num - 8);
+      }
+    }
+  }
+  return 0;
 }
--- tests/test_stego.c
+++ tests/test_stego.c
@@ -1,14 +1,69 @@
 #include "test_stego.h"
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stego.h>
 
-START_TEST(test_gen_keys)
-{
-  ck_assert_int_eq(3, 3);
+static int count_bits(uint8_t a) {
+  int ret = 0;
+  while (a) {
+    ret++;
+    a &= a - 1; // move to next bit
+  }
+  return ret;
+}
+
+static bool scalar_product(const uint8_t* a, const uint8_t* b, size_t size) {
+  bool ret = 0;
+  for (size_t i = 0; i < size; i++) {
+    ret ^= count_bits(a[i] & b[i]) & 1;
+  }
+  return ret;
+}
+
+static bool
+norm(uint8_t* a,
+     size_t size) { // Computing the norm of a, with "size" bytes length.
+  return scalar_product(a, a, size);
+}
+
+static bool
+check_orthonormality(void* buf,
+                     int count) { // Don't check for linear independency
+  uint8_t* int_buf = (uint8_t*) buf;
+  size_t key_size = STEGO_KEY_BITS / CHAR_BIT;
+  size_t total_keys_size = count * (key_size);
+  for (size_t i = 0; i < total_keys_size; i += key_size) {
+    uint8_t* key_1 = int_buf + i;
+    if (norm(key_1, key_size) == 0) {
+      return false;
+    }
+    for (size_t j = 0; j < i; j += key_size) {
+      uint8_t* key_2 = int_buf + j;
+      if (scalar_product(key_1, key_2, key_size) == 1) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+START_TEST(test_gen_keys) {
+  uint8_t buf[16 * (STEGO_KEY_BITS / CHAR_BIT)];
+  for (int count = 12; count <= 16; count++) { // Check for 10 to 16 keys
+    for (int i = 0; i < 15; i++) {
+      stego_gen_keys(buf, count);
+      ck_assert_msg(check_orthonormality(buf, count),
+                    "Error in stego_gen_keys");
+    }
+  }
 }
 END_TEST
 
 Suite* stego_suite(void) {
   Suite* suite = suite_create("stego");
-  TCase* example_tcase = tcase_create("example");
+  TCase* example_tcase = tcase_create("stego_gen_keys");
   tcase_add_test(example_tcase, test_gen_keys);
   suite_add_tcase(suite, example_tcase);
 
