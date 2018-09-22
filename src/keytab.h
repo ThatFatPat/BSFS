2f1d25f7af7fc5d80bb7c9642d211bdb96b2586f
Iddo Shavit <idoshav@gmail.com>
2018-09-22 17:03:14 +0300

    Define Header

--- src/keytab.h
+++ src/keytab.h
@@ -1,4 +1,12 @@
 #ifndef BS_KEYTAB_H
 #define BS_KEYTAB_H
+#include "disk.h"
+#include <sys/types.h>
 
+#define KEYTAB_ENTRY_SIZE 32
+#define MAX_LEVELS 16
+
+int keytab_lookup(bs_disk_t disk, const char* pass, void* key);
+int keytab_store(bs_disk_t disk, off_t index, const char* pass,
+                 const void* key);
 #endif // BS_KEYTAB_H
