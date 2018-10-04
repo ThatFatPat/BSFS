ee247c76db04703706836fdcf7ceaedd7a9567f2
eyalnesher <eyal.nesher@gmail.com>
2018-10-04 16:08:24 +0300

    Avoid using macros that not in SPEC in header files.

--- src/keytab.h
+++ src/keytab.h
@@ -6,10 +6,7 @@
 #include <sys/types.h>
 
 #define KEYTAB_MAGIC 0xBEEFCAFE
-#define KEYTAB_MAGIC_SIZE (sizeof KEYTAB_MAGIC)
 #define MAX_LEVELS 16
-#define KEYTAB_KEY_SIZE 1
-#define KEYTAB_ENTRY_SIZE 32
 
 int keytab_lookup(bs_disk_t disk, const void* password, void* key);
 int keytab_store(bs_disk_t disk, off_t index, const char* password,
