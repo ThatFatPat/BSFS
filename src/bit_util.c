43868259d4c85eeb228191641138bc5a0d14db03
Noam Raz <noamraz8@gmail.com>
2018-10-11 22:44:07 +0300

    Implement endian bit utilities

--- src/bit_util.c
+++ src/bit_util.c
@@ -1,1 +1,15 @@
 #include "bit_util.h"
+
+#include <arpa/inet.h>
+#include <string.h>
+
+uint32_t read_big_endian(const void* buf) {
+  uint32_t big_endian;
+  memcpy(&big_endian, buf, sizeof(uint32_t));
+  return ntohl(big_endian);
+}
+
+void write_big_endian(void* buf, uint32_t host_endian) {
+  uint32_t big_endian = htonl(host_endian);
+  memcpy(buf, &big_endian, sizeof(uint32_t));
+}
