8e4d743e4311152a6fb622949640615eb50c014a
Noam Raz <noamraz8@gmail.com>
2018-10-11 23:09:07 +0300

    Add `read_big_endian` unit test

--- tests/test_bit_util.c
+++ tests/test_bit_util.c
@@ -13,11 +13,18 @@ START_TEST(test_write_big_endian) {
 }
 END_TEST
 
+START_TEST(test_read_big_endian) {
+  uint8_t buf[] = { 0xde, 0xad, 0xbe, 0xef };
+  ck_assert_uint_eq(read_big_endian(buf), 0xdeadbeef);
+}
+END_TEST
+
 Suite* bit_util_suite(void) {
   Suite* suite = suite_create("bit_util");
 
   TCase* endian_tcase = tcase_create("endian");
   tcase_add_test(endian_tcase, test_write_big_endian);
+  tcase_add_test(endian_tcase, test_read_big_endian);
   suite_add_tcase(suite, endian_tcase);
 
   return suite;
