5f315e7f71a127556282eb21f7252a6066dc9bbe
Noam Raz <noamraz8@gmail.com>
2018-10-14 22:36:29 +0300

    Add test for scalar products

--- tests/test_vector.c
+++ tests/test_vector.c
@@ -3,6 +3,20 @@
 #include "vector.h"
 #include <stdint.h>
 
+START_TEST(test_scalar_product) {
+  uint32_t vector1 = 0x3f1fee11;
+  uint32_t vector2 = 0x1e2f2387;
+
+  ck_assert(vector_scalar_product((const_vector_t) &vector1,
+                                  (const_vector_t) &vector2, sizeof(uint32_t)));
+
+  vector1 &= ~((uint32_t) 1);
+
+  ck_assert(!vector_scalar_product(
+      (const_vector_t) &vector1, (const_vector_t) &vector2, sizeof(uint32_t)));
+}
+END_TEST
+
 START_TEST(test_linear_combination) {
   uint64_t vector1 = 0xdeadbeefdeadc0de;
   uint64_t vector2 = 0x8badf00dc0decafe;
@@ -21,6 +35,10 @@ END_TEST
 
 Suite* vector_suite(void) {
   Suite* suite = suite_create("vector");
+
+  TCase* scalar_product_tcase = tcase_create("scalar_product");
+  tcase_add_test(scalar_product_tcase, test_scalar_product);
+  suite_add_tcase(suite, scalar_product_tcase);
 
   TCase* linear_combination_tcase = tcase_create("linear_combination");
   tcase_add_test(linear_combination_tcase, test_linear_combination);
