812d73b9ede7e14ccb74a991bbfe841b8c159109
Noam Raz <noamraz8@gmail.com>
2018-10-15 22:25:23 +0300

    Update enc unit test

--- tests/test_enc.c
+++ tests/test_enc.c
@@ -6,18 +6,16 @@
 
 START_TEST(test_roundtrip) {
   const char* password = "Doctor Who";
-  const char* plain = "But what kind of Doctor?";
+  const char plain[32] = "But what kind of Doctor?";
 
-  void* cipher = NULL;
-  size_t cipher_size = 0;
-  int status = aes_encrypt(password, strlen(password), plain, strlen(plain) + 1,
-                           &cipher, &cipher_size);
+  void* cipher = malloc(sizeof(plain));
+  int status =
+      aes_encrypt(password, strlen(password), plain, cipher, sizeof(plain));
   ck_assert_int_eq(status, 0);
 
-  void* decrypted = NULL;
-  size_t decrypted_size = 0;
-  status = aes_decrypt(password, strlen(password), cipher, cipher_size,
-                       &decrypted, &decrypted_size);
+  void* decrypted = malloc(sizeof(plain));
+  status =
+      aes_decrypt(password, strlen(password), cipher, decrypted, sizeof(plain));
   ck_assert_int_eq(status, 0);
 
   ck_assert_str_eq(plain, (char*) decrypted);
@@ -27,40 +25,12 @@ START_TEST(test_roundtrip) {
 }
 END_TEST
 
-START_TEST(test_roundup_buffer) {
-  const char* password = "Doctor Who";
-  const char* plain = "But what kind of Doctor?";
-
-  void* cipher = NULL;
-  size_t cipher_size = 0;
-  int status = aes_encrypt(password, strlen(password), plain, strlen(plain) + 1,
-                           &cipher, &cipher_size);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(cipher_size % 16, 0);
-  ck_assert_int_ne(cipher_size, strlen(plain));
-  ck_assert_int_ne(cipher_size, 0);
-
-  free(cipher);
-}
-END_TEST
-
-START_TEST(test_get_encrypted_size) {
-  ck_assert_int_eq(aes_get_encrypted_size(8), 16);
-  ck_assert_int_eq(aes_get_encrypted_size(16), 32);
-}
-END_TEST
-
 Suite* enc_suite(void) {
   Suite* suite = suite_create("enc");
 
   TCase* roundtrip_tcase = tcase_create("roundtrip");
   tcase_add_test(roundtrip_tcase, test_roundtrip);
   suite_add_tcase(suite, roundtrip_tcase);
-
-  TCase* size_tcase = tcase_create("sizes");
-  tcase_add_test(size_tcase, test_roundup_buffer);
-  tcase_add_test(size_tcase, test_get_encrypted_size);
-  suite_add_tcase(suite, size_tcase);
 
   return suite;
 }
