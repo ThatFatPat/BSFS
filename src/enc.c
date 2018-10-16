923f63b6dfb363479e24e5b25a60b89548e89706
Noam Raz <noamraz8@gmail.com>
2018-10-16 16:49:16 +0300

    Refactor common logic into separate function

--- src/enc.c
+++ src/enc.c
@@ -26,35 +26,54 @@ static int gen_key(const EVP_CIPHER* cipher, const void* password,
   return 0;
 }
 
+static int init_cipher_ctx(const EVP_CIPHER* cipher, const void* password,
+                           size_t password_size,
+                           int (*init_func)(EVP_CIPHER_CTX*, const EVP_CIPHER*,
+                                            ENGINE*, const uint8_t*,
+                                            const uint8_t*),
+                           EVP_CIPHER_CTX** ctx) {
+  EVP_CIPHER_CTX* tmp_ctx = EVP_CIPHER_CTX_new();
+  if (!tmp_ctx) {
+    return -ENOMEM;
+  }
+
+  uint8_t key[EVP_MAX_KEY_LENGTH];
+  uint8_t iv[EVP_MAX_IV_LENGTH];
+
+  int ret = gen_key(cipher, password, password_size, key, iv);
+  if (ret < 0) {
+    goto fail;
+  }
+
+  if (!init_func(tmp_ctx, cipher, NULL, key, iv)) {
+    ret = -EIO;
+    goto fail;
+  }
+  EVP_CIPHER_CTX_set_padding(tmp_ctx, 0);
+
+  *ctx = tmp_ctx;
+  return 0;
+
+fail:
+  EVP_CIPHER_CTX_free(tmp_ctx);
+  return ret;
+}
+
 int aes_encrypt(const void* password, size_t password_size, const void* plain,
                 void* enc, size_t size) {
   if (size % AES_BLOCK_SIZE != 0) {
     return -EINVAL;
   }
 
-  int ret = 0;
-  int content_size, final_size;
-  EVP_CIPHER_CTX* e_ctx = EVP_CIPHER_CTX_new();
-
-  if (!e_ctx) {
-    return -ENOMEM;
-  }
-
-  uint8_t key[16], iv[16];
-  ret = gen_key(EVP_aes_128_cbc(), password, password_size, key, iv);
-
+  EVP_CIPHER_CTX* e_ctx;
+  int ret = init_cipher_ctx(EVP_aes_128_cbc(), password, password_size,
+                            EVP_EncryptInit_ex, &e_ctx);
   if (ret < 0) {
-    goto cleanup;
+    return ret;
   }
 
-  if (!EVP_EncryptInit_ex(e_ctx, EVP_aes_128_cbc(), NULL, key, iv)) {
-    ret = -EIO;
-    goto cleanup;
-  }
-  EVP_CIPHER_CTX_set_padding(e_ctx, 0);
-
-  content_size = 0;
-  final_size = 0;
+  int content_size = 0;
+  int final_size = 0;
 
   if (!EVP_EncryptUpdate(e_ctx, (uint8_t*) enc, &content_size,
                          (const uint8_t*) plain, size)) {
@@ -82,29 +101,15 @@ int aes_decrypt(const void* password, size_t password_size, const void* enc,
     return -EINVAL;
   }
 
-  int ret = 0;
-  int content_size, final_size;
-  EVP_CIPHER_CTX* d_ctx = EVP_CIPHER_CTX_new();
-
-  if (!d_ctx) {
-    return -ENOMEM;
-  }
-
-  uint8_t key[16], iv[16];
-  ret = gen_key(EVP_aes_128_cbc(), password, password_size, key, iv);
-
+  EVP_CIPHER_CTX* d_ctx;
+  int ret = init_cipher_ctx(EVP_aes_128_cbc(), password, password_size,
+                            EVP_DecryptInit_ex, &d_ctx);
   if (ret < 0) {
-    goto cleanup;
+    return ret;
   }
 
-  if (!EVP_DecryptInit_ex(d_ctx, EVP_aes_128_cbc(), NULL, key, iv)) {
-    ret = -EIO;
-    goto cleanup;
-  }
-  EVP_CIPHER_CTX_set_padding(d_ctx, 0);
-
-  content_size = 0;
-  final_size = 0;
+  int content_size = 0;
+  int final_size = 0;
 
   if (!EVP_DecryptUpdate(d_ctx, (uint8_t*) plain, &content_size,
                          (const uint8_t*) enc, size)) {
