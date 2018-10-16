b8d484be9a1bb78c5fd8fb93296c10df31553eee
Noam Raz <noamraz8@gmail.com>
2018-10-16 18:30:56 +0300

    Implement authenticated AES

--- src/enc.c
+++ src/enc.c
@@ -133,3 +133,84 @@ cleanup:
   EVP_CIPHER_CTX_free(d_ctx);
   return ret;
 }
+
+int aes_encrypt_auth(const void* password, size_t password_size,
+                     const void* plain, void* enc, size_t size, void* tag,
+                     size_t tag_size) {
+  EVP_CIPHER_CTX* e_ctx;
+  int ret = init_cipher_ctx(EVP_aes_128_gcm(), password, password_size,
+                            EVP_EncryptInit_ex, &e_ctx);
+  if (ret < 0) {
+    return ret;
+  }
+
+  int content_size = 0;
+  int final_size = 0;
+
+  if (!EVP_EncryptUpdate(e_ctx, (uint8_t*) enc, &content_size,
+                         (const uint8_t*) plain, size)) {
+    ret = -EIO;
+    goto cleanup;
+  }
+
+  if (!EVP_EncryptFinal_ex(e_ctx, (uint8_t*) enc + content_size, &final_size)) {
+    ret = -EIO;
+    goto cleanup;
+  }
+
+  if ((size_t)(content_size + final_size) != size) {
+    // technically, this should be impossible
+    ret = -EIO;
+    goto cleanup;
+  }
+
+  if (!EVP_CIPHER_CTX_ctrl(e_ctx, EVP_CTRL_GCM_GET_TAG, tag_size, tag)) {
+    ret = -EIO;
+  }
+
+cleanup:
+  EVP_CIPHER_CTX_free(e_ctx);
+  return ret;
+}
+
+int aes_decrypt_auth(const void* password, size_t password_size,
+                     const void* enc, void* plain, size_t size, const void* tag,
+                     size_t tag_size) {
+  EVP_CIPHER_CTX* d_ctx;
+  int ret = init_cipher_ctx(EVP_aes_128_gcm(), password, password_size,
+                            EVP_DecryptInit_ex, &d_ctx);
+  if (ret < 0) {
+    return ret;
+  }
+
+  int content_size = 0;
+  int final_size = 0;
+
+  if (!EVP_DecryptUpdate(d_ctx, (uint8_t*) plain, &content_size,
+                         (const uint8_t*) enc, size)) {
+    ret = -EIO;
+    goto cleanup;
+  }
+
+  if (!EVP_CIPHER_CTX_ctrl(d_ctx, EVP_CTRL_GCM_SET_TAG, tag_size,
+                           (void*) tag)) {
+    ret = -EIO;
+    goto cleanup;
+  }
+
+  if (EVP_DecryptFinal_ex(d_ctx, (uint8_t*) plain + content_size,
+                          &final_size) <= 0) {
+    // authentication failed
+    ret = -EBADMSG;
+    goto cleanup;
+  }
+
+  if ((size_t)(content_size + final_size) != size) {
+    // technically, this should be impossible
+    ret = -EIO;
+  }
+
+cleanup:
+  EVP_CIPHER_CTX_free(d_ctx);
+  return ret;
+}
