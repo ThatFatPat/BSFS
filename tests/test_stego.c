e0420fc5ed40f6352177bf9a12d9bbdb49fcc1ed
Iddo Shavit <idoshav@gmail.com>
2018-10-19 17:17:58 +0300

    Enhance test for compute_level_size

--- tests/test_stego.c
+++ tests/test_stego.c
@@ -55,6 +55,8 @@ START_TEST(test_compute_level_size) {
   for (size_t disk_size = 0x400; disk_size <= 0x200000; disk_size += 0x8000) {
     size_t level_size = compute_level_size(disk_size);
     ck_assert_uint_le(KEYTAB_SIZE + COVER_FILE_COUNT * level_size, disk_size);
+    ck_assert_uint_le(disk_size,
+                      KEYTAB_SIZE + COVER_FILE_COUNT * (level_size + 1));
   }
 }
 END_TEST
