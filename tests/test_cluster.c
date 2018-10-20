ccc1bdb50058515ce4eb9a6638714d16a4bada4a
Noam Raz <noamraz8@gmail.com>
2018-10-20 15:53:21 +0300

    Add unit test for `compute_bitmap_size`

--- tests/test_cluster.c
+++ tests/test_cluster.c
@@ -2,6 +2,7 @@
 
 #include "bft.h"
 #include "cluster.h"
+#include <limits.h>
 
 START_TEST(test_count_clusters) {
   for (size_t level_size = BFT_SIZE; level_size < 0x200000;
@@ -14,11 +15,24 @@ START_TEST(test_count_clusters) {
 }
 END_TEST
 
+START_TEST(test_compute_bitmap_size) {
+  for (size_t level_size = BFT_SIZE; level_size < BFT_SIZE + 0x20000;
+       level_size += 0x800) {
+    size_t clusters = fs_count_clusters(level_size);
+    size_t bitmap_size = fs_compute_bitmap_size(level_size);
+
+    ck_assert_uint_eq(bitmap_size % 16, 0);
+    ck_assert_uint_le(clusters, bitmap_size * CHAR_BIT);
+  }
+}
+END_TEST
+
 Suite* cluster_suite(void) {
   Suite* suite = suite_create("cluster");
 
   TCase* count_tcase = tcase_create("count");
   tcase_add_test(count_tcase, test_count_clusters);
+  tcase_add_test(count_tcase, test_compute_bitmap_size);
   suite_add_tcase(suite, count_tcase);
 
   return suite;
