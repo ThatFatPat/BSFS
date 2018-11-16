3de01aef6a9fc422383ee6807438c885939c5c1a
Noam Raz <noamraz8@gmail.com>
2018-11-16 17:00:48 +0200

    Update cluster to use new stego keys

--- src/cluster.h
+++ src/cluster.h
@@ -2,6 +2,7 @@
 #define BS_CLUSTER_H
 
 #include "disk.h"
+#include "stego.h"
 #include <stddef.h>
 #include <stdint.h>
 
@@ -14,16 +15,16 @@ typedef uint32_t cluster_offset_t;
 size_t fs_count_clusters(size_t level_size);
 size_t fs_compute_bitmap_size(size_t clusters);
 
-int fs_read_cluster(const void* key, bs_disk_t disk, void* buf,
+int fs_read_cluster(const stego_key_t* key, bs_disk_t disk, void* buf,
                     cluster_offset_t cluster);
-int fs_write_cluster(const void* key, bs_disk_t disk, const void* buf,
+int fs_write_cluster(const stego_key_t* key, bs_disk_t disk, const void* buf,
                      cluster_offset_t cluster);
 
 cluster_offset_t fs_next_cluster(const void* cluster);
 void fs_set_next_cluster(void* cluster, cluster_offset_t next);
 
-int fs_read_bitmap(const void* key, bs_disk_t disk, void* buf);
-int fs_write_bitmap(const void* key, bs_disk_t disk, const void* buf);
+int fs_read_bitmap(const stego_key_t* key, bs_disk_t disk, void* buf);
+int fs_write_bitmap(const stego_key_t* key, bs_disk_t disk, const void* buf);
 
 int fs_alloc_cluster(void* bitmap, size_t bitmap_bits,
                      cluster_offset_t* new_cluster);
