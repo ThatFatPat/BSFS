9c1199f72e39904dcb38378835db442da2a664b9
Noam Raz <noamraz8@gmail.com>
2018-12-08 14:55:31 +0200

    Fix compilation errors in stego test

--- tests/test_stego.c
+++ tests/test_stego.c
@@ -15,16 +15,17 @@
 
 START_TEST(test_compute_level_size) {
   for (size_t disk_size = 0x400; disk_size <= 0x200000; disk_size += 0x8000) {
-    size_t level_size = compute_level_size(disk_size);
-    ck_assert_uint_le(KEYTAB_SIZE + COVER_FILE_COUNT * level_size, disk_size);
+    size_t level_size = stego_compute_user_level_size(disk_size);
+    ck_assert_uint_le(KEYTAB_SIZE + STEGO_COVER_FILE_COUNT * level_size,
+                      disk_size);
     ck_assert_uint_le(disk_size,
-                      KEYTAB_SIZE + COVER_FILE_COUNT * (level_size + 1));
+                      KEYTAB_SIZE + STEGO_COVER_FILE_COUNT * (level_size + 1));
   }
 }
 END_TEST
 
 START_TEST(test_compute_level_size_too_small) {
-  ck_assert_int_eq(compute_level_size(500), 0);
+  ck_assert_int_eq(stego_compute_user_level_size(500), 0);
 }
 END_TEST
 
