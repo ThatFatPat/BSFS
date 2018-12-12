7bc3f228e110d203024de25b01e0ed55a8dde4fb
nirshahar <33443445+nirshahar@users.noreply.github.com>
2018-12-12 19:33:58 +0200

    Declare matrix API

--- src/vector.h
+++ src/vector.h
@@ -8,6 +8,11 @@
 typedef uint8_t* vector_t;
 typedef const uint8_t* const_vector_t;
 
+typedef vector_t matrix_t;
+typedef const_vector_t const_matrix_t;
+
+// Vector API
+
 bool vector_scalar_product(const_vector_t a, const_vector_t b, size_t size);
 
 bool vector_norm(const_vector_t a, size_t size);
@@ -16,5 +21,13 @@ void vector_linear_combination(vector_t linear_combination,
                                const_vector_t first_vector,
                                const_vector_t second_vector, size_t vector_size,
                                bool coefficient);
+
+// Matrix API
+
+void matrix_multiply(matrix_t restrict dest, const_matrix_t a, const_matrix_t b, size_t dim);
+
+void matrix_transpose(matrix_t dest, const_matrix_t matrix, size_t dim);
+
+int matrix_gen_nonsing(matrix_t mat, matrix_t inv, size_t dim);
 
 #endif // BS_VECTOR_H
