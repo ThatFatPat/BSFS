dbb1d4ab8bdff0387311217a2f6bbc99cd37761b
Noam Raz <noamraz8@gmail.com>
2018-12-24 22:27:50 +0200

    Restore old stego unit tests

--- tests/test_stego.c
+++ tests/test_stego.c
@@ -77,6 +77,74 @@ START_TEST(test_compute_level_size_too_small) {
 }
 END_TEST
 
+static bs_disk_t create_tmp_disk(size_t size) {
+  int fd = syscall(SYS_memfd_create, "test_stego.bsf", 0);
+  ck_assert_int_ne(ftruncate(fd, size), -1);
+
+  bs_disk_t disk;
+  ck_assert_int_eq(disk_create(fd, &disk), 0);
+  return disk;
+}
+
+START_TEST(test_read_write_level_roundtrip) {
+  stego_key_t keys[2];
+  ck_assert_int_eq(stego_gen_user_keys(keys, 2), 0);
+
+  char data1[32] = "Hello!!! This is level 1!";
+  char data2[32] = "And this is level 2!";
+
+  char read1[sizeof(data1)] = { 0 };
+  char read2[sizeof(data2)] = { 0 };
+
+  bs_disk_t disk = create_tmp_disk(0x200000); // 2MiB
+
+  ck_assert_int_eq(stego_write_level(keys, disk, data1, 0, sizeof(data1)), 0);
+  ck_assert_int_eq(
+      stego_write_level(keys + STEGO_KEY_SIZE, disk, data2, 0, sizeof(data2)),
+      0);
+
+  ck_assert_int_eq(stego_read_level(keys, disk, read1, 0, sizeof(read1)), 0);
+  ck_assert_int_eq(memcmp(read1, data1, sizeof(data1)), 0);
+
+  ck_assert_int_eq(
+      stego_read_level(keys + STEGO_KEY_SIZE, disk, read2, 0, sizeof(read2)),
+      0);
+  ck_assert_int_eq(memcmp(read2, data2, sizeof(data2)), 0);
+
+  disk_free(disk);
+}
+END_TEST
+
+START_TEST(test_read_write_invalid) {
+  stego_key_t key;
+  ck_assert_int_eq(stego_gen_user_keys(&key, 1), 0);
+
+  char data[25] = "Incorrectly-sized data";
+  char correct_data[16] = "correctly-sized";
+
+  char read[25];
+
+  bs_disk_t disk = create_tmp_disk(0x200000); // 2MiB
+
+  ck_assert_int_eq(stego_write_level(&key, disk, data, 0, sizeof(data)),
+                   -EINVAL);
+
+  ck_assert_int_eq(
+      stego_write_level(&key, disk, correct_data, 0, sizeof(correct_data)), 0);
+  ck_assert_int_eq(
+      stego_write_level(&key, disk, correct_data, 5, sizeof(correct_data)),
+      -EINVAL);
+
+  ck_assert_int_eq(stego_read_level(&key, disk, read, 0, sizeof(read)),
+                   -EINVAL);
+
+  ck_assert_int_eq(stego_read_level(&key, disk, read, 0, 16), 0);
+  ck_assert_int_eq(stego_read_level(&key, disk, read, 5, 16), -EINVAL);
+
+  disk_free(disk);
+}
+END_TEST
+
 Suite* stego_suite(void) {
   Suite* suite = suite_create("stego");
 
@@ -89,6 +157,11 @@ Suite* stego_suite(void) {
   tcase_add_test(level_size_tcase, test_compute_level_size);
   tcase_add_test(level_size_tcase, test_compute_level_size_too_small);
   suite_add_tcase(suite, level_size_tcase);
+
+  TCase* stego_read_write_tcase = tcase_create("read_write");
+  tcase_add_test(stego_read_write_tcase, test_read_write_level_roundtrip);
+  tcase_add_test(stego_read_write_tcase, test_read_write_invalid);
+  suite_add_tcase(suite, stego_read_write_tcase);
 
   return suite;
 }
