595b663d858e4835d5b011ee5e5e1c050514b922
Noam Raz <noamraz8@gmail.com>
2019-01-10 19:03:41 +0200

    Incorporate salting into AES unit test

--- tests/test_enc.c
+++ tests/test_enc.c
@@ -9,18 +9,27 @@ START_TEST(test_basic_roundtrip) {
   const char* password = "Doctor Who";
   const char plain[32] = "But what kind of Doctor?";
 
+  const char salt[] = "salt!!!";
+  const char bad_salt[] = "bad salt";
+
   char cipher[sizeof(plain)];
   char decrypted[sizeof(plain)];
 
-  int status = aes_encrypt(password, strlen(password), NULL, 0, plain, cipher,
-                           sizeof(plain));
+  int status = aes_encrypt(password, strlen(password), salt, sizeof(salt),
+                           plain, cipher, sizeof(plain));
   ck_assert_int_eq(status, 0);
 
-  status = aes_decrypt(password, strlen(password), NULL, 0, cipher, decrypted,
-                       sizeof(cipher));
+  status = aes_decrypt(password, strlen(password), salt, sizeof(salt), cipher,
+                       decrypted, sizeof(cipher));
   ck_assert_int_eq(status, 0);
 
   ck_assert_str_eq(plain, (char*) decrypted);
+
+  status = aes_decrypt(password, strlen(password), bad_salt, sizeof(bad_salt),
+                       cipher, decrypted, sizeof(cipher));
+  ck_assert_int_eq(status, 0);
+
+  ck_assert_int_ne(memcmp(decrypted, plain, sizeof(plain)), 0);
 }
 END_TEST
 
