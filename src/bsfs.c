c51371ffe6023863423e8074c6ecbd4f203a642a
ThatFatPat <idoshav@gmail.com>
2019-01-13 20:09:27 +0200

    Add stubs for fuse_ops in bsfs

--- src/bsfs.c
+++ src/bsfs.c
@@ -3,6 +3,7 @@
 #include "bft.h"
 #include "disk.h"
 #include "stego.h"
+#include <errno.h>
 #include <pthread.h>
 
 struct bs_file_impl {
@@ -28,3 +29,59 @@ struct bs_bsfs_impl {
   bs_disk_t disk;
   pthread_mutex_t level_lock;
 };
+
+int bsfs_init(int fd, bs_bsfs_t* fs) {
+  return -ENOSYS;
+}
+
+void bsfs_destry(bs_bsfs_t fs) {
+}
+
+int bsfs_mknod(bs_bsfs_t fs, const char* path) {
+  return -ENOSYS;
+}
+
+int bsfs_unlink(bs_bsfs_t fs, const char* path) {
+  return -ENOSYS;
+}
+
+int bsfs_open(bs_bsfs_t fs, const char* path, bs_file_t* file) {
+  return -ENOSYS;
+}
+
+int bsfs_release(bs_file_t file) {
+  return -ENOSYS;
+}
+
+ssize_t bsfs_read(bs_file_t file, void* buf, size_t size, off_t off) {
+  return -ENOSYS;
+}
+
+ssize_t bsfs_write(bs_file_t file, const void* buf, size_t size, off_t off) {
+  return -ENOSYS;
+}
+
+int bsfs_getattr(bs_bsfs_t fs, const char* path, struct stat* st) {
+  return -ENOSYS;
+}
+
+int bsfs_setattr(bs_bsfs_t fs, const char* path, const struct stat* st) {
+  return -ENOSYS;
+}
+
+int bsfs_rename(bs_bsfs_t fs, const char* src_path, const char* new_name,
+                unsigned int flags) {
+  return -ENOSYS;
+}
+
+int bsfs_chmod(bs_bsfs_t fs, const char* path, mode_t mode) {
+  return -ENOSYS;
+}
+
+int bsfs_truncate(bs_bsfs_t fs, const char* path, off_t size) {
+  return -ENOSYS;
+}
+
+int readdir(bs_bsfs_t fs, const char* name, bs_dir_iter_t iter) {
+  return -ENOSYS;
+}
--- src/bsfs.h
+++ src/bsfs.h
@@ -1,10 +1,35 @@
 #ifndef BS_BSFS_H
 #define BS_BSFS_H
 
+#include "bft.h"
 #include <stddef.h>
+#include <sys/stat.h>
 
 typedef struct bs_file_impl* bs_file_t;
 typedef struct bs_open_level_impl* bs_open_level_t;
 typedef struct bs_bsfs_impl* bs_bsfs_t;
+
+int bsfs_init(int fd, bs_bsfs_t* fs);
+void bsfs_destry(bs_bsfs_t fs);
+
+int bsfs_mknod(bs_bsfs_t fs, const char* path);
+int bsfs_unlink(bs_bsfs_t fs, const char* path);
+
+int bsfs_open(bs_bsfs_t fs, const char* path, bs_file_t* file);
+int bsfs_release(bs_file_t file);
+
+ssize_t bsfs_read(bs_file_t file, void* buf, size_t size, off_t off);
+ssize_t bsfs_write(bs_file_t file, const void* buf, size_t size, off_t off);
+
+int bsfs_getattr(bs_bsfs_t fs, const char* path, struct stat* st);
+int bsfs_setattr(bs_bsfs_t fs, const char* path, const struct stat* st);
+int bsfs_rename(bs_bsfs_t fs, const char* src_path, const char* new_name,
+                unsigned int flags);
+int bsfs_chmod(bs_bsfs_t fs, const char* path, mode_t mode);
+int bsfs_truncate(bs_bsfs_t fs, const char* path, off_t size);
+
+typedef int (*bs_dir_iter_t)(const char* name, const struct stat* st,
+                             bft_offset_t off);
+int readdir(bs_bsfs_t fs, const char* name, bs_dir_iter_t iter);
 
 #endif
