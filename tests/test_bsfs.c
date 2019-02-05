3fed1cc6a9f1bcf7eeafeb311de79b35cc589d8e
Noam Raz <noamraz8@gmail.com>
2019-02-05 15:45:58 +0200

    Add open file table refcount test

--- tests/test_bsfs.c
+++ tests/test_bsfs.c
@@ -28,12 +28,30 @@ START_TEST(test_ftab_insert_size) {
 }
 END_TEST
 
+START_TEST(test_ftab_insert_lookup_refcount) {
+  bs_file_table_t table;
+  ck_assert_int_eq(bs_file_table_init(&table), 0);
+
+  bs_file_t file1;
+  ck_assert_int_eq(bs_file_table_open(&table, NULL, 123, &file1), 0);
+  ck_assert_int_eq(file1->refcount, 1);
+
+  bs_file_t file2;
+  ck_assert_int_eq(bs_file_table_open(&table, NULL, 123, &file2), 0);
+  ck_assert_ptr_eq(file1, file2);
+  ck_assert_int_eq(file2->refcount, 2);
+
+  bs_file_table_destroy(&table);
+}
+END_TEST
+
 Suite* bsfs_suite(void) {
   Suite* suite = suite_create("bsfs");
 
   TCase* ftab_tcase = tcase_create("ftab");
   tcase_add_test(ftab_tcase, test_ftab_insert_lookup);
   tcase_add_test(ftab_tcase, test_ftab_insert_size);
+  tcase_add_test(ftab_tcase, test_ftab_insert_lookup_refcount);
   suite_add_tcase(suite, ftab_tcase);
 
   return suite;
