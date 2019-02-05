7fd18fb0ec8cc13e76307a1a75fc9ab85492256a
Noam Raz <noamraz8@gmail.com>
2019-02-05 15:32:45 +0200

    Create file table insertion tests

--- tests/test_bsfs.c
+++ tests/test_bsfs.c
@@ -1,16 +1,40 @@
 #include "test_bsfs.h"
 
-START_TEST(test_example) {
-  ck_assert_int_eq(3, 3);
+#include "bsfs_priv.h"
+
+START_TEST(test_ftab_insert_lookup) {
+  bs_file_table_t table;
+  ck_assert_int_eq(bs_file_table_init(&table), 0);
+
+  bs_file_t created;
+  ck_assert_int_eq(bs_file_table_open(&table, NULL, 123, &created), 0);
+  bs_file_t found;
+  ck_assert_int_eq(bs_file_table_open(&table, NULL, 123, &found), 0);
+  ck_assert_ptr_eq(created, found);
+
+  bs_file_table_destroy(&table);
+}
+END_TEST
+
+START_TEST(test_ftab_insert_size) {
+  bs_file_table_t table;
+  ck_assert_int_eq(bs_file_table_init(&table), 0);
+
+  bs_file_t file;
+  ck_assert_int_eq(bs_file_table_open(&table, NULL, 123, &file), 0);
+  ck_assert_uint_eq(table.size, 1);
+
+  bs_file_table_destroy(&table);
 }
 END_TEST
 
 Suite* bsfs_suite(void) {
   Suite* suite = suite_create("bsfs");
-  TCase* example_tcase = tcase_create("example");
 
-  tcase_add_test(example_tcase, test_example);
-  suite_add_tcase(suite, example_tcase);
+  TCase* ftab_tcase = tcase_create("ftab");
+  tcase_add_test(ftab_tcase, test_ftab_insert_lookup);
+  tcase_add_test(ftab_tcase, test_ftab_insert_size);
+  suite_add_tcase(suite, ftab_tcase);
 
   return suite;
 }
