a4db1510b9c05b58e8fd1f3ff5baba1c61c0f6c5
Noam Raz <noamraz8@gmail.com>
2019-03-07 22:35:21 +0200

    Create `level_get` test

--- tests/test_bsfs.c
+++ tests/test_bsfs.c
@@ -33,7 +33,32 @@ START_TEST(test_bsfs_init_disk_too_small) {
 }
 END_TEST
 
-START_TEST(test_get_level) {
+static bs_bsfs_t tmp_fs;
+
+stego_key_t level_get_keys[2];
+static const char* level_get_pass1 = "pass1";
+static const char* level_get_pass2 = "pass2";
+
+static void level_get_fs_setup(void) {
+  ck_assert_int_eq(stego_gen_user_keys(level_get_keys, 2), 0);
+  int fd = create_tmp_file(FS_DISK_SIZE);
+  ck_assert_int_eq(bsfs_init(fd, &tmp_fs), 0);
+  ck_assert_int_eq(
+      keytab_store(tmp_fs->disk, 0, level_get_pass1, level_get_keys), 0);
+  ck_assert_int_eq(
+      keytab_store(tmp_fs->disk, 1, level_get_pass2, level_get_keys + 1), 0);
+}
+
+static void level_get_fs_teardown(void) {
+  bsfs_destroy(tmp_fs);
+}
+
+START_TEST(test_level_get) {
+  bs_open_level_t level1;
+  ck_assert_int_eq(bs_level_get(tmp_fs, level_get_pass1, &level1), 0);
+  ck_assert_int_eq(memcmp(&level1->key, level_get_keys, sizeof(stego_key_t)),
+                   0);
+  ck_assert_str_eq(level1->pass, level_get_pass1);
 }
 END_TEST
 
@@ -44,6 +69,12 @@ Suite* bsfs_suite(void) {
   tcase_add_test(init_tcase, test_bsfs_init);
   tcase_add_test(init_tcase, test_bsfs_init_disk_too_small);
   suite_add_tcase(suite, init_tcase);
+
+  TCase* level_get_tcase = tcase_create("level_get");
+  tcase_add_checked_fixture(level_get_tcase, level_get_fs_setup,
+                            level_get_fs_teardown);
+  tcase_add_test(level_get_tcase, test_level_get);
+  suite_add_tcase(suite, level_get_tcase);
 
   return suite;
 }
