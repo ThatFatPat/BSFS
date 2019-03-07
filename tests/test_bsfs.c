c75014b735886a77f80ea392e61842c865812d52
Noam Raz <noamraz8@gmail.com>
2019-03-07 21:20:24 +0200

    Add filesystem initialization tests

--- tests/test_bsfs.c
+++ tests/test_bsfs.c
@@ -1,7 +1,37 @@
 #include "test_bsfs.h"
 
+#include "bft.h"
 #include "bsfs_priv.h"
+#include "keytab.h"
+#include "stego.h"
 #include <errno.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#define FS_DISK_SIZE                                                           \
+  (KEYTAB_SIZE + STEGO_USER_LEVEL_COUNT * (BFT_SIZE + 0x1000))
+
+static int create_tmp_file(size_t size) {
+  int fd = syscall(SYS_memfd_create, "test_bsfs.bsf", 0);
+  ck_assert_int_ne(ftruncate(fd, size), -1);
+  return fd;
+}
+
+START_TEST(test_bsfs_init) {
+  int fd = create_tmp_file(FS_DISK_SIZE);
+  bs_bsfs_t fs;
+  ck_assert_int_eq(bsfs_init(fd, &fs), 0);
+  bsfs_destroy(fs);
+}
+END_TEST
+
+START_TEST(test_bsfs_init_disk_too_small) {
+  int fd = create_tmp_file(5);
+  bs_bsfs_t fs;
+  ck_assert_int_eq(bsfs_init(fd, &fs), -ENOSPC);
+  close(fd);
+}
+END_TEST
 
 START_TEST(test_get_level) {
 }
@@ -9,6 +39,11 @@ END_TEST
 
 Suite* bsfs_suite(void) {
   Suite* suite = suite_create("bsfs");
+
+  TCase* init_tcase = tcase_create("init");
+  tcase_add_test(init_tcase, test_bsfs_init);
+  tcase_add_test(init_tcase, test_bsfs_init_disk_too_small);
+  suite_add_tcase(suite, init_tcase);
 
   return suite;
 }
