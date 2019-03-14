ac14342952c374f8028b0c21dda605be05e39963
ThatFatPat <idoshav@gmail.com>
2019-03-14 11:12:42 +0200

    Fix possible deadlock

--- src/bsfs.c
+++ src/bsfs.c
@@ -542,6 +542,15 @@ int bsfs_rename(bs_bsfs_t fs, const char* src_path, const char* new_path,
     return ret;
   }
 
+  char* name;
+  char* pass;
+
+  ret = bs_split_path(src_path, &pass, &name);
+  if (ret < 0) {
+    ret = -ENOENT;
+    goto cleanup_after_old_alloc;
+  }
+
   bs_open_level_t new_path_level;
   bft_offset_t new_path_index;
   bool new_path_exists;
@@ -554,30 +563,22 @@ int bsfs_rename(bs_bsfs_t fs, const char* src_path, const char* new_path,
     goto cleanup;
   }
 
-  int ret_new_file = get_locked_level_and_index(
-      fs, new_path, false, &new_path_level, &new_path_index);
-  pthread_rwlock_unlock(&level->metadata_lock);
-  if (ret_new_file < 0) {
-    new_path_exists = false;
-  }
-
-  if (!(&level == &new_path_level)) {
+  if (!(&pass == &new_pass)) {
     ret = -EXDEV;
     goto cleanup_after_new_alloc;
+  }
+
+  int ret_new_file =
+      bft_find_table_entry(level->bft, new_name, &new_path_index);
+  if (ret_new_file < 0) {
+    new_path_exists = false;
   }
 
   if (new_path_index) {
     new_path_exists = true;
   }
-  // Logic: Implement RENAME_EXCHANGE case here.
-  char* name;
-  char* pass;
 
-  ret = bs_split_path(src_path, &pass, &name);
-  if (ret < 0) {
-    ret = -ENOENT;
-    goto cleanup_after_old_alloc;
-  }
+  // Logic: Implement RENAME_EXCHANGE case here.
   if (flags & RENAME_EXCHANGE) {
     ret = exchange_filenames(level, index, new_path_index);
     goto cleanup_after_old_alloc;
