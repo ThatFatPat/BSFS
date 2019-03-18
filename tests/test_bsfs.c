607ec7d2025319188f5ade4438cf472ca2dcf73b
Noam Raz <noamraz8@gmail.com>
2019-03-18 17:39:55 +0200

    Add test for cross-level renames

--- tests/test_bsfs.c
+++ tests/test_bsfs.c
@@ -306,6 +306,12 @@ START_TEST(test_rename_noent) {
 }
 END_TEST
 
+START_TEST(test_rename_cross_level) {
+  ck_assert_int_eq(bsfs_rename(tmp_fs, "renamelvl/file1", "otherlvl/file1", 0),
+                   -EXDEV);
+}
+END_TEST
+
 START_TEST(test_rename_noreplace) {
   char path[256];
   strcpy(path, rename_level_name);
@@ -437,6 +443,7 @@ Suite* bsfs_suite(void) {
   tcase_add_checked_fixture(rename_tcase, rename_fs_setup, rename_fs_teardown);
   tcase_add_test(rename_tcase, test_rename);
   tcase_add_test(rename_tcase, test_rename_noent);
+  tcase_add_test(rename_tcase, test_rename_cross_level);
   tcase_add_test(rename_tcase, test_rename_noreplace);
   tcase_add_test(rename_tcase, test_rename_whiteout);
   tcase_add_test(rename_tcase, test_rename_replace);
