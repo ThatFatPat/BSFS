466cc82b8ce4f471f9360acbb62eccabc2a9a530
Noam Raz <noamraz8@gmail.com>
2019-03-25 22:47:26 +0200

    Implement init and destroy

--- src/fuse_ops.c
+++ src/fuse_ops.c
@@ -1,9 +1,32 @@
 #include "fuse_ops.h"
 
+#include "bsfs.h"
+#include <stdbool.h>
+#include <unistd.h>
+
+#define LONG_TIMEOUT 500000 // Arbitrarily long timeout
+
 int bsfs_fuse_init(struct fuse_conn_info* conn, struct fuse_config* cfg) {
+  conn->time_gran = 1000000000; // 1 second
+  conn->want |= FUSE_CAP_WRITEBACK_CACHE;
+
+  cfg->set_uid = true;
+  cfg->uid = getuid();
+
+  cfg->set_gid = true;
+  cfg->gid = getgid();
+
+  // The filesystem can't be changed externally when mounted, so let the kernel
+  // cache everything.
+  cfg->kernel_cache = true;
+  cfg->attr_timeout = LONG_TIMEOUT;
+  cfg->entry_timeout = LONG_TIMEOUT;
+
+  return 0;
 }
 
 void bsfs_fuse_destroy(void* private_data) {
+  bsfs_destroy((bs_bsfs_t) private_data);
 }
 
 int bsfs_fuse_mknod(const char* path, mode_t mode, dev_t dev) {
