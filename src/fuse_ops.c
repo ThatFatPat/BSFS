3e79e74b1ad5059cfd27e288a4840088681bae27
Noam Raz <noamraz8@gmail.com>
2019-03-25 23:30:41 +0200

    Implement FUSE operation wrappers

--- src/fuse_ops.c
+++ src/fuse_ops.c
@@ -46,43 +46,80 @@ void bsfs_fuse_destroy(void* private_data) {
 }
 
 int bsfs_fuse_mknod(const char* path, mode_t mode, dev_t dev) {
+  (void) dev;
+  return bsfs_mknod(get_fs(), path, mode);
 }
 
 int bsfs_fuse_unlink(const char* path) {
+  return bsfs_unlink(get_fs(), path);
 }
 
 int bsfs_fuse_open(const char* path, struct fuse_file_info* fi) {
+  bs_file_t file;
+  int ret = bsfs_open(get_fs(), path, &file);
+  if (ret < 0) {
+    return ret;
+  }
+  set_file(fi, file);
+  return 0;
+}
+
+int bsfs_fuse_opendir(const char* path, struct fuse_file_info* fi) {
+  // All checks will be performed by the kernel - just make sure there's no
+  // associated BSFS file so that `getattr` isn't confused.
+  (void) path;
+  set_file(fi, NULL);
+  return 0;
 }
 
 int bsfs_fuse_release(const char* path, struct fuse_file_info* fi) {
+  (void) path;
+  return bsfs_release(get_file(fi));
 }
 
 int bsfs_fuse_read(const char* path, char* buf, size_t size, off_t off,
                    struct fuse_file_info* fi) {
+  (void) path;
+  return bsfs_read(get_file(fi), buf, size, off);
 }
 
 int bsfs_fuse_write(const char* path, const char* buf, size_t size, off_t off,
                     struct fuse_file_info* fi) {
+  (void) path;
+  return bsfs_write(get_file(fi), buf, size, off);
 }
 
 int bsfs_fuse_fsync(const char* path, int datasync, struct fuse_file_info* fi) {
+  (void) path;
+  return bsfs_fsync(get_file(fi), datasync);
 }
 
 int bsfs_fuse_getattr(const char* path, struct stat* st,
                       struct fuse_file_info* fi) {
+  bs_file_t file = try_get_file(fi);
+  return file ? bsfs_fgetattr(file, st) : bsfs_getattr(get_fs(), path, st);
 }
 
 int bsfs_fuse_chmod(const char* path, mode_t mode, struct fuse_file_info* fi) {
+  bs_file_t file = try_get_file(fi);
+  return file ? bsfs_fchmod(file, mode) : bsfs_chmod(get_fs(), path, mode);
 }
 
 int bsfs_fuse_truncate(const char* path, off_t size,
                        struct fuse_file_info* fi) {
+  bs_file_t file = try_get_file(fi);
+  return file ? bsfs_ftruncate(file, size)
+              : bsfs_truncate(get_fs(), path, size);
 }
 
 int bsfs_fuse_utimens(const char* path, const struct timespec times[2],
                       struct fuse_file_info* fi) {
+  bs_file_t file = try_get_file(fi);
+  return file ? bsfs_futimens(file, times)
+              : bsfs_utimens(get_fs(), path, times);
 }
 
 int bsfs_fuse_rename(const char* oldpath, const char* newpath,
                      unsigned int flags) {
+  return bsfs_rename(get_fs(), oldpath, newpath, flags);
 }
