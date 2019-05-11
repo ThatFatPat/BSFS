b978b49c28e0bd53914a66c1b2821a17da053f70
Noam Raz <noamraz8@gmail.com>
2019-05-11 16:02:01 +0300

    Define fuse operations

--- src/main.c
+++ src/main.c
@@ -26,6 +26,24 @@ static int bsfs_opt_proc(void* data, const char* arg, int key,
   return 1;
 }
 
+static const struct fuse_operations ops = { .init = bsfs_fuse_init,
+                                            .destroy = bsfs_fuse_destroy,
+                                            .mknod = bsfs_fuse_mknod,
+                                            .unlink = bsfs_fuse_unlink,
+                                            .open = bsfs_fuse_open,
+                                            .release = bsfs_fuse_release,
+                                            .opendir = bsfs_fuse_opendir,
+                                            .read = bsfs_fuse_read,
+                                            .write = bsfs_fuse_write,
+                                            .fsync = bsfs_fuse_fsync,
+                                            .getattr = bsfs_fuse_getattr,
+                                            .chmod = bsfs_fuse_chmod,
+                                            .truncate = bsfs_fuse_truncate,
+                                            .utimens = bsfs_fuse_utimens,
+                                            .fallocate = bsfs_fuse_fallocate,
+                                            .rename = bsfs_fuse_rename,
+                                            .readdir = bsfs_fuse_readdir };
+
 int main(int argc, char* argv[]) {
   struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
   struct bsfs_config config = { 0 };
