722f0ce1f692cc7d05f9ec89a4e203b617adb6f6
Noam Raz <noamraz8@gmail.com>
2019-05-30 15:26:19 +0300

    Create new AES API

--- src/enc.c
+++ src/enc.c
@@ -1,5 +1,6 @@
 #include "enc.h"
 
+#include <assert.h>
 #include <errno.h>
 #include <openssl/aes.h>
 #include <openssl/evp.h>
@@ -8,29 +9,11 @@
 
 #define NROUNDS 10000
 
-static int gen_key(const EVP_CIPHER* cipher, const void* password,
-                   size_t password_size, const void* salt, size_t salt_size,
-                   void* key, void* iv) {
-  uint8_t key_iv[EVP_MAX_KEY_LENGTH + EVP_MAX_IV_LENGTH];
-
-  int keylen = EVP_CIPHER_key_length(cipher);
-  int ivlen = EVP_CIPHER_iv_length(cipher);
-
-  int ret = enc_key_from_bytes(password, password_size, salt, salt_size,
-                               NROUNDS, keylen + ivlen, key_iv);
-  if (ret < 0) {
-    return ret;
-  }
-
-  memcpy(key, key_iv, keylen);
-  memcpy(iv, key_iv + keylen, ivlen);
-
-  return 0;
-}
+static_assert(ENC_BLOCK_SIZE == AES_BLOCK_SIZE,
+              "Incorrect block size constant");
 
 static int
-init_cipher_ctx(const EVP_CIPHER* cipher, const void* password,
-                size_t password_size, const void* salt, size_t salt_size,
+init_cipher_ctx(const EVP_CIPHER* cipher, const void* key, const void* iv,
                 int (*init_func)(EVP_CIPHER_CTX*, const EVP_CIPHER*, ENGINE*,
                                  const uint8_t*, const uint8_t*),
                 EVP_CIPHER_CTX** ctx) {
@@ -39,26 +22,14 @@ init_cipher_ctx(const EVP_CIPHER* cipher, const void* password,
     return -ENOMEM;
   }
 
-  uint8_t key[EVP_MAX_KEY_LENGTH];
-  uint8_t iv[EVP_MAX_IV_LENGTH];
-
-  int ret = gen_key(cipher, password, password_size, salt, salt_size, key, iv);
-  if (ret < 0) {
-    goto fail;
-  }
-
   if (!init_func(tmp_ctx, cipher, NULL, key, iv)) {
-    ret = -EIO;
-    goto fail;
+    EVP_CIPHER_CTX_free(tmp_ctx);
+    return -EIO;
   }
   EVP_CIPHER_CTX_set_padding(tmp_ctx, 0);
 
   *ctx = tmp_ctx;
   return 0;
-
-fail:
-  EVP_CIPHER_CTX_free(tmp_ctx);
-  return ret;
 }
 
 int enc_key_from_bytes(const void* password, size_t password_size,
@@ -71,15 +42,15 @@ int enc_key_from_bytes(const void* password, size_t password_size,
              : -EIO;
 }
 
-int aes_encrypt(const void* password, size_t password_size, const void* salt,
-                size_t salt_size, const void* plain, void* enc, size_t size) {
-  if (size % AES_BLOCK_SIZE != 0) {
+int aes_encrypt(const void* key, const void* iv, const void* plain, void* enc,
+                size_t size) {
+  if (size % ENC_BLOCK_SIZE != 0) {
     return -EINVAL;
   }
 
   EVP_CIPHER_CTX* e_ctx;
-  int ret = init_cipher_ctx(EVP_aes_128_cbc(), password, password_size, salt,
-                            salt_size, EVP_EncryptInit_ex, &e_ctx);
+  int ret =
+      init_cipher_ctx(EVP_aes_128_cbc(), key, iv, EVP_EncryptInit_ex, &e_ctx);
   if (ret < 0) {
     return ret;
   }
@@ -108,15 +79,15 @@ cleanup:
   return ret;
 }
 
-int aes_decrypt(const void* password, size_t password_size, const void* salt,
-                size_t salt_size, const void* enc, void* plain, size_t size) {
-  if (size % AES_BLOCK_SIZE != 0) {
+int aes_decrypt(const void* key, const void* iv, const void* enc, void* plain,
+                size_t size) {
+  if (size % ENC_BLOCK_SIZE != 0) {
     return -EINVAL;
   }
 
   EVP_CIPHER_CTX* d_ctx;
-  int ret = init_cipher_ctx(EVP_aes_128_cbc(), password, password_size, salt,
-                            salt_size, EVP_DecryptInit_ex, &d_ctx);
+  int ret =
+      init_cipher_ctx(EVP_aes_128_cbc(), key, iv, EVP_DecryptInit_ex, &d_ctx);
   if (ret < 0) {
     return ret;
   }
@@ -146,12 +117,11 @@ cleanup:
   return ret;
 }
 
-int aes_encrypt_auth(const void* password, size_t password_size,
-                     const void* salt, size_t salt_size, const void* plain,
+int aes_encrypt_auth(const void* key, const void* iv, const void* plain,
                      void* enc, size_t size, void* tag, size_t tag_size) {
   EVP_CIPHER_CTX* e_ctx;
-  int ret = init_cipher_ctx(EVP_aes_128_gcm(), password, password_size, salt,
-                            salt_size, EVP_EncryptInit_ex, &e_ctx);
+  int ret =
+      init_cipher_ctx(EVP_aes_128_gcm(), key, iv, EVP_EncryptInit_ex, &e_ctx);
   if (ret < 0) {
     return ret;
   }
@@ -185,13 +155,12 @@ cleanup:
   return ret;
 }
 
-int aes_decrypt_auth(const void* password, size_t password_size,
-                     const void* salt, size_t salt_size, const void* enc,
+int aes_decrypt_auth(const void* key, const void* iv, const void* enc,
                      void* plain, size_t size, const void* tag,
                      size_t tag_size) {
   EVP_CIPHER_CTX* d_ctx;
-  int ret = init_cipher_ctx(EVP_aes_128_gcm(), password, password_size, salt,
-                            salt_size, EVP_DecryptInit_ex, &d_ctx);
+  int ret =
+      init_cipher_ctx(EVP_aes_128_gcm(), key, iv, EVP_DecryptInit_ex, &d_ctx);
   if (ret < 0) {
     return ret;
   }
