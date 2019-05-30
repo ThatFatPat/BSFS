5f80a51769ba484ae5909596f7c85bf2101ee22b
Noam Raz <noamraz8@gmail.com>
2019-05-30 15:51:26 +0300

    Update key table to use new API

--- src/keytab.c
+++ src/keytab.c
@@ -8,6 +8,14 @@
 #include <string.h>
 
 #define KEYTAB_KEY_ENTRY_SIZE (KEYTAB_ENTRY_SIZE - KEYTAB_TAG_SIZE)
+#define KEYTAB_KEY_IV_SIZE (ENC_AUTH_KEY_SIZE + ENC_AUTH_IV_SIZE)
+#define KEYTAB_PBKDF_ITER 10000
+
+static int gen_key_iv(const void* password, size_t password_len,
+                      const void* salt, void* buf) {
+  return enc_key_from_bytes(password, password_len, salt, KEYTAB_SALT_SIZE,
+                            KEYTAB_PBKDF_ITER, KEYTAB_KEY_IV_SIZE, buf);
+}
 
 int keytab_lookup(bs_disk_t disk, const char* password, stego_key_t* key) {
   // Get the key table
@@ -27,30 +35,36 @@ int keytab_lookup(bs_disk_t disk, const char* password, stego_key_t* key) {
   const uint8_t* keytab_data = keytab + KEYTAB_SALT_SIZE;
 
   size_t password_len = strlen(password);
-  uint8_t key_buf[KEYTAB_KEY_ENTRY_SIZE]; // Will hold serialized key
+  uint8_t ent[KEYTAB_KEY_ENTRY_SIZE]; // Will hold serialized key
 
   // Search for key in table
   for (size_t i = 0; i < STEGO_USER_LEVEL_COUNT; i++) {
     const uint8_t* encrypted_ent = keytab_data + i * KEYTAB_ENTRY_SIZE;
 
-    int decrypt_status = aes_decrypt_auth(
-        password, password_len, salt, KEYTAB_SALT_SIZE, encrypted_ent, key_buf,
-        KEYTAB_KEY_ENTRY_SIZE, encrypted_ent + KEYTAB_KEY_ENTRY_SIZE,
-        KEYTAB_TAG_SIZE);
+    uint8_t key_iv[KEYTAB_KEY_IV_SIZE];
+    int ret = gen_key_iv(password, password_len, salt, key_iv);
+    if (ret < 0) {
+      return ret;
+    }
 
-    if (decrypt_status == 0) {
+    ret = aes_decrypt_auth(key_iv, key_iv + ENC_AUTH_KEY_SIZE, encrypted_ent,
+                           ent, KEYTAB_KEY_ENTRY_SIZE,
+                           encrypted_ent + KEYTAB_KEY_ENTRY_SIZE,
+                           KEYTAB_TAG_SIZE);
+
+    if (ret == 0) {
       // Deserialize the key
-      memcpy(key->aes_key, key_buf, sizeof(key->aes_key));
-      memcpy(key->read_keys, key_buf + sizeof(key->aes_key),
+      memcpy(key->aes_key, ent, sizeof(key->aes_key));
+      memcpy(key->read_keys, ent + sizeof(key->aes_key),
              sizeof(key->read_keys));
       memcpy(key->write_keys,
-             key_buf + sizeof(key->aes_key) + sizeof(key->read_keys),
+             ent + sizeof(key->aes_key) + sizeof(key->read_keys),
              sizeof(key->write_keys));
     }
 
-    if (decrypt_status != -EBADMSG) {
+    if (ret != -EBADMSG) {
       // Either the key was found or there was an unknown error
-      return decrypt_status;
+      return ret;
     }
   }
 
@@ -73,25 +87,29 @@ int keytab_store(bs_disk_t disk, off_t index, const char* password,
   const void* salt = keytab;
   uint8_t* keytab_data = (uint8_t*) keytab + KEYTAB_SALT_SIZE;
 
-  uint8_t key_buf[KEYTAB_KEY_ENTRY_SIZE]; // Will hold serialized key
+  uint8_t ent[KEYTAB_KEY_ENTRY_SIZE]; // Will hold serialized key
 
   // Serialize the key
-  memcpy(key_buf, key->aes_key, sizeof(key->aes_key));
-  memcpy(key_buf + sizeof(key->aes_key), key->read_keys,
-         sizeof(key->read_keys));
-  memcpy(key_buf + sizeof(key->aes_key) + sizeof(key->read_keys),
-         key->write_keys, sizeof(key->write_keys));
+  memcpy(ent, key->aes_key, sizeof(key->aes_key));
+  memcpy(ent + sizeof(key->aes_key), key->read_keys, sizeof(key->read_keys));
+  memcpy(ent + sizeof(key->aes_key) + sizeof(key->read_keys), key->write_keys,
+         sizeof(key->write_keys));
+
+  uint8_t key_iv[KEYTAB_KEY_IV_SIZE];
+  int ret = gen_key_iv(password, strlen(password), salt, key_iv);
 
   // Store the key in the key table
-  uint8_t ent[KEYTAB_ENTRY_SIZE];
-  int ret = aes_encrypt_auth(password, strlen(password), salt, KEYTAB_SALT_SIZE,
-                             key_buf, ent, KEYTAB_KEY_ENTRY_SIZE,
-                             ent + KEYTAB_KEY_ENTRY_SIZE, KEYTAB_TAG_SIZE);
+  uint8_t encrypted_ent[KEYTAB_ENTRY_SIZE];
+  ret =
+      aes_encrypt_auth(key_iv, key_iv + ENC_AUTH_KEY_SIZE, ent, encrypted_ent,
+                       KEYTAB_KEY_ENTRY_SIZE,
+                       encrypted_ent + KEYTAB_KEY_ENTRY_SIZE, KEYTAB_TAG_SIZE);
   if (ret < 0) {
     goto cleanup;
   }
 
-  memcpy(keytab_data + index * KEYTAB_ENTRY_SIZE, ent, KEYTAB_ENTRY_SIZE);
+  memcpy(keytab_data + index * KEYTAB_ENTRY_SIZE, encrypted_ent,
+         KEYTAB_ENTRY_SIZE);
 
 cleanup:
   disk_unlock_write(disk);
