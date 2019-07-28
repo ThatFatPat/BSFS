2b863756bce64735a85d850557d45b5df939ae89
Noam Raz <noamraz8@gmail.com>
2019-07-28 13:11:35 +0300

    Report errors in invocation of fusermount

--- src/main/main.c
+++ src/main/main.c
@@ -158,6 +158,8 @@ int main(int argc, char* argv[]) {
     const char* fusermount_args[] = { "fusermount3", "-u", config.disk_path,
                                       NULL };
     execvp("fusermount3", (char**) fusermount_args);
+    perror("error: failed to invoke fusermount3");
+    return 2;
   }
 
   if (!config.disk_path) {
